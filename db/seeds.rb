b622b2a07f750007c33f45d3e7bf4a7067bbe033
orikazu <orikazu.0824@i.softbank.jp>
2019-07-12 10:51:03 +0000

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-post1=Post.new(content: "今日からProgateでRailsの勉強するよー！")
-post1.save
-post2=Post.new(content: "投稿一覧ページ作成中！")
-post2.save
+if Post.count == 0
+  post1=Post.new(content: "今日からProgateでRailsの勉強するよー！")
+  post1.save
+  post2=Post.new(content: "投稿一覧ページ作成中！")
+  post2.save
+end
 

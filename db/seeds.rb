2302fb51335d14b408ffa1978daeb30fe5050348
orikazu <orikazu.0824@i.softbank.jp>
2019-07-12 10:40:06 +0000

    db作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post1=Post.new(content: "今日からProgateでRailsの勉強するよー！")
+post1.save
+post2=Post.new(content: "投稿一覧ページ作成中！")
+post2.save
+
